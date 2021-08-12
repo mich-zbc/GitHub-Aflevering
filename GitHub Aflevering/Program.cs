add502e68f43469fec96c30453ae3b069321aab4
Michael Olesen <mich@zbc.dk>
2021-08-12 09:07:19 +0200

    Add project files.

---
+++ GitHub Aflevering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub_Aflevering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
